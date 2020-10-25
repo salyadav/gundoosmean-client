ca23cfed8989e29117e79a9aa2964c9cf26d8752
NathanPang001 <54946176+NathanPang001@users.noreply.github.com>
2020-10-25 19:56:15 -0400

    Update index.js

--- ui/js/index.js
+++ ui/js/index.js
@@ -38,7 +38,7 @@ document.getElementById('showRulesBtn').addEventListener('click', showRules);
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', () => 
+window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
